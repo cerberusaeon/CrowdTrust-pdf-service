c5cd35fab48855a6e7d7ce60dfa795431b777ae9
Stephen Ohimor <saodesign@gmail.com>
2013-04-02 15:06:23 -0400

    added log4j
    import sql isn't getting picked up by hsql

---
+++ astrum-common/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT into PERSONS (personId, firstName, lastName, dateOfBirth) values (1001, 'Julie', 'Lambis', '1996-02-06 00:00:00.000');
+
+INSERT into PERSONS (personId, firstName, lastName, dateOfBirth) values (1002, 'Tom', 'Crew', '1986-02-06 00:00:00.000');
