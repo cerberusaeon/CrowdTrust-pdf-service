3f96f17e5b57c2d74ee8a03b7c742b42e53f172a
Stephen Ohimor <saodesign@gmail.com>
2013-04-04 21:11:18 -0400

    fixed import xml test working

--- astrum-common/src/main/java/org/astrum/common/dao/PersonDAO.java
+++ astrum-common/src/main/java/org/astrum/common/dao/PersonDAO.java
@@ -1,16 +1,12 @@
 package org.astrum.common.dao;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Random;
-
-import javax.persistence.EntityManager;
 
 import org.astrum.common.domain.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Repository;
-@Repository
+
 public class PersonDAO extends BaseDAO {
 
 	protected static Logger logger = LoggerFactory.getLogger(PersonDAO.class);
@@ -26,28 +22,15 @@ public class PersonDAO extends BaseDAO {
 		p.setFirstName("Bob"+new Random().nextInt());
 		p.setLastName("Dylan");
 		
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
+	/*	EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 		entityManager.persist(p);
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		logger.debug("~~~~~~~~~~~~~~~~~~~ check~~~~~~~~~~~~~~~~~~~~~~");
-		logger.info("leaving savePerson");
+		logger.info("leaving savePerson");*/
 		return p;
 	}
 	
-	public Person readPerson(){
-		Person person = null;
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		List<Person> result = entityManager.createQuery( "from Person", Person.class ).getResultList();
-		for ( Person p : result ) {
-		    System.out.println(p);
-		    person = p;
-		}
-		entityManager.getTransaction().commit();
-		entityManager.close();
-		
-		return person;
-	}
+	
 }
--- astrum-common/src/test/java/org/astrum/common/dao/PersonDAOTest.java
+++ astrum-common/src/test/java/org/astrum/common/dao/PersonDAOTest.java
@@ -1,12 +1,15 @@
 package org.astrum.common.dao;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.astrum.common.domain.Person;
+import org.astrum.common.repository.PersonRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import javax.inject.Inject;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "classpath:applicationContext.xml",
@@ -14,16 +17,19 @@ import javax.inject.Inject;
 public class PersonDAOTest {
 
 	@Inject
-	private PersonDAO personDAO;
+	private PersonRepository personRepository;
 	
 	@Test
 	public void testPersonDAO(){
-		PersonDAO dao = new PersonDAO();
-		dao.savePerson(new Person());
-		dao.savePerson(new Person());
-		dao.savePerson(new Person());
-		dao.savePerson(new Person());
-		dao.readPerson();
+		
+		personRepository.save(new PersonDAO().savePerson(new Person()));
+		personRepository.save(new PersonDAO().savePerson(new Person()));
+		personRepository.save(new PersonDAO().savePerson(new Person()));
+		personRepository.save(new PersonDAO().savePerson(new Person()));
+		List<Person>  people = personRepository.findAll();
+		for(Person p: people){
+			System.out.println("Person: "+p);
+		}
 		
 	}
 }
