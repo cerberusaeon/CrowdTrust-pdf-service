5f4887cd3e9fec9f24f58e74bd30ff621666ea93
Stephen Ohimor <sohimor@dokkit.com>
2013-04-10 23:06:03 -0400

    spring inject services + test
    added js components libraries and giving boiler plate a try

--- astrum-services/src/main/java/org/astrum/services/rest/UserRestService.java
+++ astrum-services/src/main/java/org/astrum/services/rest/UserRestService.java
@@ -1,16 +1,26 @@
-package org.astrum.services.rest.model;
+package org.astrum.services.rest;
 
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-@Path("/user")
-public class UserService {
+import org.astrum.common.repository.PersonRepository;
 
+@Path("/user")
+public class UserRestService {
+
+	
+	@Inject
+	PersonRepository personRepository;
 	
 	@GET
 	@Path("/{userId}")
+	@Produces("application/json")
+	@Consumes("application/json")
 	public Response getMsg(@PathParam("userId") String msg) {
  
 		String output = "Astrum says : " + msg;
